9fd7579d66671308cd204a01018a7f6a192760f3
desarrolladorjuniorservialco <desarrollador.junior@servialco.com>
2026-04-24 20:12:56 -0500

    eliminar proyecto

--- src/components/SectionBadge.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const COLORES_VALIDOS = new Set(['blue', 'green', 'red', 'orange', 'yellow', 'purple', 'teal']);
-
-export default function SectionBadge({ label, color = 'blue' }) {
-  const safeColor = COLORES_VALIDOS.has(color) ? color : 'blue';
-  return <div className={`section-badge sb-${safeColor}`}>{label}</div>;
-}
--- src/pages/ComponenteAmbiental.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ComponenteBase from './_ComponenteBase';
-
-export default function ComponenteAmbiental({ perfil }) {
-  return <ComponenteBase perfil={perfil} titulo="Componente Ambiental - SST" color="green" componente="ambiental" />;
-}
